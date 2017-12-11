eea5bc323a83dc32dee16cafc73f829dac4cc956
megadotnet <wintersunair@gmail.com>
2017-12-11 19:57:06 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Springboot Demo 
+A Sample application developed with Spring Boot, Hibernate and Angular JS.
+Application is secured with JWT and Spring security.
+Application includes:
+   1) Standard login and logout
+   2) Registration and email verification
+   3) Remember me functionality
+   4) Forgot password
+   5) Multi-language support
+  
---
+++ src/test/java/com/app/UserServiceTest.java
@@ -0,0 +1,39 @@
+package com.app;
+
+import com.app.login.Application;
+import com.app.login.domain.User;
+import com.app.login.service.UserService;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.Instant;
+
+import static org.junit.Assert.assertNotNull;
+
+@Ignore
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class UserServiceTest {
+
+	@Autowired
+	UserService userService;
+
+	@Test
+	public void createUserTest() {
+		User user = new User();
+		user.setFirstName("Bruce");
+		user.setLastName("Wayne");
+		user.setPassword("batman");
+		user.setCreatedDate(Instant.now());
+		user.setEmail("bruce@dccomics.com");
+		user.setIpAddress("127.0.0.1");
+
+		User newUser = userService.createUser(user.getFirstName(), user.getPassword(), user.getFirstName(), user.getLastName(),
+				user.getEmail(), "", "en", user.getCreatedDate(), user.getIpAddress());
+		assertNotNull(newUser);
+	}
+}
