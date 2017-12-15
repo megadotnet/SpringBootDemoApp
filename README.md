5ac21f745ff90b89ce2bdfdd4cb00da60a3ea6bf
megadotnet <wintersunair@gmail.com>
2017-12-15 14:55:54 +0800

    Updated SpringBoot version to 1.59

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Springboot Demo 
-A Sample application developed with Spring Boot, Hibernate and Angular JS.
+A Sample application developed with Spring Boot, Spring Security, Hibernate, Actuator and Angular JS
 Application is secured with JWT and Spring security.
-Application includes:
+
+
+Feature includes:
    1) Standard login and logout
    2) Registration and email verification
    3) Remember me functionality
--- src/main/java/com/app/login/Application.java
+++ src/main/java/com/app/login/Application.java
@@ -1,16 +1,20 @@
 package com.app.login;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
+import com.app.login.config.Constants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
 
-import com.app.login.config.Constants;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
+/**
+ * Application
+ * @author megadotnet
+ * @date 2017-12-15
+ */
 @SpringBootApplication
 public class Application {
 
