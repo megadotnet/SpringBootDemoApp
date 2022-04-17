ea5fbeca7f07a039f9536883ed87a15aa0115d13
megadotnet <wintersunair@gmail.com>
2022-04-17 17:33:56 +0800

    adjust DOS_MAX_REGISTRATION_ALLOWED for testing

--- src/main/java/com/app/login/config/Constants.java
+++ src/main/java/com/app/login/config/Constants.java
@@ -12,7 +12,7 @@ public final class Constants {
     public static final Long TOKEN_VALIDITY_IN_MILLISECONDS = 1800000L;
     public static final Long TOKEN_VALIDITY_IN_MILLISECONDS_FOR_REMEMBER_ME = 604800000L;
     public static final String DEFAULT_LANGUAGE_KEY = "en";
-    public static final Integer DOS_MAX_REGISTRATION_ALLOWED = 3;
+    public static final Integer DOS_MAX_REGISTRATION_ALLOWED = 300;
     private static final String NEWLINE = "\n----------------------------------------------------------\n\t";
     private static final String APP_MSG = "Application '{}' is running! Access URLs:\n\t";
     private static final String EXT = "External: \t{}://{}:{}\n\t";
