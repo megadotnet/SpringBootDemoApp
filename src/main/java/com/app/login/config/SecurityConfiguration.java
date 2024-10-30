6450240e4534342277a45d86aa62ab2ccfee6abf
megadotnet <wintersunair@gmail.com>
2024-10-30 16:52:22 +0800

    features:updated for springboot 2.7

--- src/main/java/com/app/login/config/SecurityConfiguration.java
+++ src/main/java/com/app/login/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.BeanInitializationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -35,32 +36,23 @@ import com.app.login.security.jwt.TokenProvider;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@DependsOn("userDetailsService")
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    private final AuthenticationManagerBuilder authenticationManagerBuilder;
-
     private final UserDetailsService userDetailsService;
-
     private final TokenProvider tokenProvider;
-
     private final CorsFilter corsFilter;
 
-    public SecurityConfiguration(AuthenticationManagerBuilder authenticationManagerBuilder, UserDetailsService userDetailsService, TokenProvider tokenProvider, CorsFilter corsFilter) {
-
-        this.authenticationManagerBuilder = authenticationManagerBuilder;
+    @Autowired
+    public SecurityConfiguration(UserDetailsService userDetailsService, TokenProvider tokenProvider, CorsFilter corsFilter) {
         this.userDetailsService = userDetailsService;
         this.tokenProvider = tokenProvider;
         this.corsFilter = corsFilter;
     }
 
-    @PostConstruct
-    public void init() {
-        try {
-            authenticationManagerBuilder.userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder());
-        } catch (Exception e) {
-            throw new BeanInitializationException("Security configuration failed", e);
-        }
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
     @Bean
@@ -71,51 +63,46 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring()
-            .antMatchers(HttpMethod.OPTIONS, "/**")
-            .antMatchers("/app/**/*.{js,html}")
-            .antMatchers("/bower_components/**")
-            .antMatchers("/i18n/**")
-            .antMatchers("/content/**")
-            .antMatchers("/test/**")
-            .antMatchers("/h2-console/**");
+                .antMatchers(HttpMethod.OPTIONS, "/**")
+                .antMatchers("/app/**/*.{js,html}")
+                .antMatchers("/bower_components/**")
+                .antMatchers("/i18n/**")
+                .antMatchers("/content/**")
+                .antMatchers("/test/**")
+                .antMatchers("/h2-console/**");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
-            .exceptionHandling()
-            .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-            .and()
-            .csrf()
-            .disable()
-            .headers()
-            .frameOptions()
-            .disable()
-            .and()
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
-            .authorizeRequests()
-            .antMatchers("/api/register")
-            .permitAll()
-            .antMatchers("/api/activate")
-            .permitAll()
-            .antMatchers("/api/authenticate")
-            .permitAll()
-            .antMatchers("/api/account/reset_password/init")
-            .permitAll()
-            .antMatchers("/api/account/reset_password/finish")
-            .permitAll()
-            .antMatchers("/api/**")
-            .authenticated()
-               //.antMatchers("/v2/api-docs").authenticated()
-                //.and()
-                //.httpBasic();
-            .and()
-            .apply(securityConfigurerAdapter());
-
+                .exceptionHandling()
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                .and()
+                .csrf()
+                .disable()
+                .headers()
+                .frameOptions()
+                .disable()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/register")
+                .permitAll()
+                .antMatchers("/api/activate")
+                .permitAll()
+                .antMatchers("/api/authenticate")
+                .permitAll()
+                .antMatchers("/api/account/reset_password/init")
+                .permitAll()
+                .antMatchers("/api/account/reset_password/finish")
+                .permitAll()
+                .antMatchers("/api/**")
+                .authenticated()
+                .and()
+                .apply(securityConfigurerAdapter());
     }
-
 
     private JWTConfigurer securityConfigurerAdapter() {
         return new JWTConfigurer(tokenProvider);
@@ -131,3 +118,4 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         return authenticationManager();
     }
 }
+
