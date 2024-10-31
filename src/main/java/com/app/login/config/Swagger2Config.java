61274f5a9464dd8ce7580ee0d8712dfef7967925
megadotnet <wintersunair@gmail.com>
2024-10-31 11:34:10 +0800

    features: updated for         <dependency>                                   <groupId>io.springfox</groupId>                                   <artifactId>springfox-swagger2</artifactId>                                   <version>2.10.5</version>                               </dependency>

--- src/main/java/com/app/login/config/Swagger2Config.java
+++ src/main/java/com/app/login/config/Swagger2Config.java
@@ -9,13 +9,15 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
 
 /**
+ * Swagger2Config
  * Created by Administrator on 2018/6/3 0003.
  */
 @Configuration
-@EnableSwagger2
+@EnableSwagger2WebMvc
 @Profile("!prod")
 public class Swagger2Config {
     @Bean
